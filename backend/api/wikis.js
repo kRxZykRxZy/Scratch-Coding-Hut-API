daebfd45be51f54573e8f947d855ae8c03320e87
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-03-03 17:24:09 +0000

    Create wikis.js

---
+++ backend/api/wikis.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const wikiSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Wiki = mongoose.model('Wiki', wikiSchema);
+
+app.get('/api/wikis', async (req, res) => {
+  const wikis = await Wiki.find();
+  res.json(wikis);
+});
+
+app.post('/api/wikis', async (req, res) => {
+  const newWiki = new Wiki(req.body);
+  await newWiki.save();
+  res.json(newWiki);
+});
+
+app.put('/api/wikis/:id', async (req, res) => {
+  const updatedWiki = await Wiki.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedWiki);
+});
+
+app.delete('/api/wikis/:id', async (req, res) => {
+  await Wiki.findByIdAndDelete(req.params.id);
+  res.sendStatus(204);
+});
+
+module.exports = app;
